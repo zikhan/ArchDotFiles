db564792766be6548cc8ea58b23774dbdb329232
Zovin Khanmohammed <zovinkhanmohd@hotmail.com>
2020-10-18 18:56:03 -0500

    Don't hardcode the repo directory

--- CreateSymLinks.sh
+++ CreateSymLinks.sh
@@ -1,42 +1,43 @@
 #!/bin/bash
 
-configFiles=( .xinitrc
-.Xmodmap
-.xscreensaver
-.zprofile
-.zshrc
-.config/cava/config
-.config/tilda/config_cava
-.config/xfce4/terminal/terminalrc
-.config/xfce4/panel/whiskermenu-1.rc
-.config/xfce4/xfconf/xfce-perchannel-xml/displays.xml
-.config/xfce4/xfconf/xfce-perchannel-xml/keyboards.xml
-.config/xfce4/xfconf/xfce-perchannel-xml/pointers.xml
-.config/xfce4/xfconf/xfce-perchannel-xml/ristretto.xml
-.config/xfce4/xfconf/xfce-perchannel-xml/thunar.xml
-.config/xfce4/xfconf/xfce-perchannel-xml/thunar-volman.xml
-.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml
-.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
-.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-notifyd.xml
-.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
-.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml
-.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-session.xml
-.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
-.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
-.config/yay/config.json
-.conky/conky-startup.sh
-.stellarium/config.ini
-".conky/4 & 2 core blue/conkyrc2core"
-'.themes/Circela Mod/xfwm4/*' )
+configFiles=(
+	.xinitrc
+	.Xmodmap
+	.xscreensaver
+	.zprofile
+	.zshrc
+	.config/cava/config
+	.config/tilda/config_cava
+	.config/xfce4/terminal/terminalrc
+	.config/xfce4/panel/whiskermenu-1.rc
+	.config/xfce4/xfconf/xfce-perchannel-xml/displays.xml
+	.config/xfce4/xfconf/xfce-perchannel-xml/keyboards.xml
+	.config/xfce4/xfconf/xfce-perchannel-xml/pointers.xml
+	.config/xfce4/xfconf/xfce-perchannel-xml/ristretto.xml
+	.config/xfce4/xfconf/xfce-perchannel-xml/thunar.xml
+	.config/xfce4/xfconf/xfce-perchannel-xml/thunar-volman.xml
+	.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml
+	.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
+	.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-notifyd.xml
+	.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
+	.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml
+	.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-session.xml
+	.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
+	.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
+	.config/yay/config.json
+	.conky/conky-startup.sh
+	.stellarium/config.ini
+	".conky/4 & 2 core blue/conkyrc2core"
+	'.themes/Circela Mod/xfwm4/*'
+)
 
 IFS=""
 
 for f in ${configFiles[*]}
 do
     printf "Working on: %s\n" $f
-    # cp --parents "$f" "/git/ArchDotFiles/"
-    ln -sf "/home/zovin/git/ArchDotFiles/$f" "$HOME/$f"
+    ln -sf "$PWD/$f" "$HOME/$f"
 done
 
 # Move RunCava.sh to usr/bin directory
-sudo ln -sf "/home/zovin/git/ArchDotFiles/RunCava.sh" "/usr/bin/RunCava.sh"
+sudo ln -sf "$PWD/RunCava.sh" "/usr/bin/RunCava.sh"
