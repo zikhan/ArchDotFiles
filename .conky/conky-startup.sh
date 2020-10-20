9b6a34923e728da2667ea57abb9bde5ba649c8ee
Zovin Khanmohammed <zovinkhanmohd@hotmail.com>
2020-10-19 23:15:17 -0500

    Upgrade Conky. Make it work with Matcha-dark-azul

--- .conky/conky-startup.sh
+++ .conky/conky-startup.sh
@@ -1,4 +1,5 @@
 sleep 10s
 killall conky
 cd "/home/zovin/.conky/4 & 2 core blue"
-conky -c "/home/zovin/.conky/4 & 2 core blue/conkyrc2core" &
+conky -c "/home/zovin/.conky/4 & 2 core blue/conkyrc2core" --daemonize
+exit 0
